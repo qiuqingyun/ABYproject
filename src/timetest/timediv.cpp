6c5a84bffb27c2b94c16aaef440660df1393411b
qqy <809969480@qq.com>
2021-02-23 12:30:30 +0800

    2.23

--- src/timetest/timediv.cpp
+++ src/timetest/timediv.cpp
@@ -50,125 +50,43 @@ void read_test_options(int32_t *argcp, char ***argvp, e_role *role,
 }
 
 void test(e_role role, const std::string &address, uint16_t port, seclvl seclvl, uint32_t nvals, uint32_t nthreads,
-		  e_mt_gen_alg mt_alg, e_sharing sharing, int choose)
+		  e_mt_gen_alg mt_alg, e_sharing sharing,int choose)
 {
 	// for addition we operate on doubles, so set bitlen to 64 bits
 	uint32_t bitlen = 32;
 	ABYParty *party = new ABYParty(role, address, port, seclvl, bitlen, nthreads, mt_alg, 100000);
 	std::vector<Sharing *> &sharings = party->GetSharings();
-	Circuit *circ = (Circuit *)sharings[sharing]->GetCircuitBuildRoutine();
-
-	uint32_t opta = 48, optb = 12, ans;
-	share *s_opta = circ->PutINGate(opta, bitlen, SERVER);
-	share *s_optb = circ->PutINGate(optb, bitlen, SERVER);
-	
+	//Circuit *circ = (Circuit *)sharings[sharing]->GetCircuitBuildRoutine();
+    BooleanCircuit *bool_circ = (BooleanCircuit *)sharings[S_BOOL]->GetCircuitBuildRoutine();
 
     float f_opta = 48.0F,f_optb = 12.0F,fans = 0.0F;
 	uint32_t *faPtr = (uint32_t *)&f_opta;
 	uint32_t *fbPtr = (uint32_t *)&f_optb;
-	share *s_fopta = circ->PutINGate(faPtr,bitlen,SERVER);
-	share *s_foptb = circ->PutINGate(fbPtr,bitlen,SERVER);
+	share *s_fopta = bool_circ->PutINGate(faPtr,bitlen,SERVER);
+	share *s_foptb = bool_circ->PutINGate(fbPtr,bitlen,SERVER);
 	share *s_ans, *s_out;
 	//share *s_fans,*s_fout;
-
-	switch (choose)
-	{
-	case 0:
-		s_ans = circ->PutADDGate(s_opta, s_optb);
-		ans = opta + optb;
-		break;
-	case 1:
-		s_ans = circ->PutSUBGate(s_opta, s_optb);
-		ans = opta - optb;
-		break;
-	case 2:
-		s_ans = circ->PutMULGate(s_opta, s_optb);
-		ans = opta * optb;
-		break;
-	case 3:
-	{
-		Circuit *yc = sharings[S_YAO]->GetCircuitBuildRoutine();
-		Circuit *bc = sharings[S_BOOL]->GetCircuitBuildRoutine();
-		bc->PutA2BGate(s_opta, yc);
-		std::cout << "| A2B |" << std::endl;
-	}
-	break;
-	case 4:
-	{
-		Circuit *ac = sharings[S_ARITH]->GetCircuitBuildRoutine();
-		ac->PutB2AGate(s_opta);
-		std::cout << "| B2A |" << std::endl;
-	}
-	break;
-	case 5:
-	{
-		s_ans = circ->PutGTGate(s_opta, s_optb);
-		ans = (opta > optb) ? 1 : 0;
-	}
-	break;
-    case 6:
-    {
-        s_ans = circ->PutADDGate(s_opta,s_optb);
-        ans = opta + optb;
-        std::cout << "| BS_SecADD |" << std::endl;
-    }
-    break;
-    case 7:
-    {
-        s_ans = circ->PutSUBGate(s_opta,s_optb);
-        ans = opta - optb;
-        std::cout << "| BS_SecSUB |" << std::endl;
-    }
-    break;
-    case 8:
-    {
-        s_ans = circ->PutMULGate(s_opta,s_optb);
-        ans = opta * optb;
-        std::cout << "| BS_SecMUL |" << std::endl;
-    }
-    break;
-	case 9:
-    {
-       // s_ans = circ->PutFPGate(s_fopta,s_foptb,DIV);//PutFPGate
-	     s_ans=circ->PutFPGate(s_fopta,s_foptb,DIV);
+    // s_ans = circ->PutFPGate(s_fopta,s_foptb,DIV);//PutFPGate
+	s_ans=bool_circ->PutFPGate(s_fopta,s_foptb,DIV);
 	   // s_ans = circ->PutMULGate(s_opta,s_optb);
-        fans = f_opta / f_optb;
-        std::cout << "| BS_SecDIV |" << std::endl;
-    }
-    break;
-	default:
-		exit(1);
-		break;
-	}
-	if (choose < 3 || choose > 4)
-		s_out = circ->PutOUTGate(s_ans, ALL);
+    fans = f_opta / f_optb;
+    std::cout << "| BS_SecDIV |" << std::endl;
+
+    s_out = bool_circ->PutOUTGate(s_ans, ALL);
 
 	// run
 	party->ExecCircuit();
-	if (choose < 3 || (choose > 4 && choose < 9))
-	{
-		uint32_t out_val = s_out->get_clear_value<uint32_t>();
-		std::cout << "-------------------------------" << std::endl;
-		if (choose == 5)
-			choose -= 2;
-        if(choose == 6 || choose == 7 || choose == 8){
-            choose -= 6;
-        }
-		std::string flags[4] = {"+", "-", "×", ">"};
-		std::cout << "| " << opta << flags[choose] << optb << " | ans: " << out_val << " | check: " << ans<< " |" << std::endl;
-		std::cout << "-------------------------------\n"
-				  << std::endl;
-	}
+	
 
-	if(choose == 9){
-        uint32_t *s_out_out = (uint32_t *)s_out->get_clear_value_ptr();
-	    float s_out_out_f = *((float *)s_out_out);
+    uint32_t *s_out_out = (uint32_t *)s_out->get_clear_value_ptr();
+	float s_out_out_f = *((float *)s_out_out);
 
-		std::cout << "-------------------------------" << std::endl;
-		std::cout << "| " << f_opta << "/"<< f_optb << " | ans: " << s_out_out_f << " | check: " << fans<< " |" << std::endl;
-		std::cout << "-------------------------------\n"
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << "| " << f_opta << "/"<< f_optb << " | ans: " << s_out_out_f << " | check: " << fans<< " |" << std::endl;
+	std::cout << "-------------------------------\n"
 				  << std::endl;
-	}
+	
+
 }
  
 
@@ -186,9 +104,12 @@ int main(int argc, char **argv)
 
 	read_test_options(&argc, &argv, &role, &bitlen, &nvals, &secparam, &address,
 					  &port, &test_op, &test_bit, &choose);
+    // read_test_options(&argc, &argv, &role, &bitlen, &nvals, &secparam, &address,
+	// 				  &port, &test_op, &test_bit);
 
 	seclvl seclvl = get_sec_lvl(secparam);
 	e_sharing sharing = choose > 3 ? S_BOOL : S_ARITH;
-	test(role, address, port, seclvl, nvals, nthreads, mt_alg, sharing, choose);
+	//test(role, address, port, seclvl, nvals, nthreads, mt_alg, sharing, choose);
+    test(role, address, port, seclvl, nvals, nthreads, mt_alg, S_BOOL,choose);
 	return 0;
 }
